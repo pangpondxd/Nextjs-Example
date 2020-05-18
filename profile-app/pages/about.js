9eef31f5d5718a56330c6e904ea9aae3881b547d
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2020-05-18 12:28:27 +0700

    add function

---
+++ profile-app/pages/about.js
@@ -0,0 +1,14 @@
+
+//functional component - arrow function
+const About = () => (
+    <h1>Hello About Page</h1>
+)
+
+//functional component  - normal function
+// function About() {
+//     return (
+//         <h1>Hello About Page</h1>
+//     )
+// }
+
+export default About
