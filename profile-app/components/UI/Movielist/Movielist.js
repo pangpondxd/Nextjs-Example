ba31d3f9e94fb06ed0fc9525174241c645aa1f03
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2020-06-07 19:06:57 +0700

    add Link and page.id

--- profile-app/components/UI/Movielist/Movielist.js
+++ profile-app/components/UI/Movielist/Movielist.js
@@ -1,5 +1,5 @@
-import react from 'react'
-
+import Link from 'next/link'
+import React from 'react'
 class MovieList extends React.Component{
 
     shorten = (text, maxLength) => {
@@ -13,10 +13,14 @@ class MovieList extends React.Component{
             (
                 <div key={movie.id} className="col-lg-4 col-md-6 mb-4">
                 <div className="card h-100">
-                  <a href="#"><img className="card-img-top" src={movie.image} alt="" /></a>
+                <Link href={`/movies/${movie.id}`}>
+                  <a><img className="card-img-top" src={movie.image} alt="" /></a>
+                  </Link>
                   <div className="card-body">
                     <h4 className="card-title">
-                      <a href="#">{movie.name}</a>
+                    <Link href={`/movies/${movie.id}`}>
+                      <a>{movie.name}</a>
+                      </Link>
                     </h4>
                     <h5>$24.99</h5>
             <p className="card-text">{this.shorten(movie.description, 50)}</p>
