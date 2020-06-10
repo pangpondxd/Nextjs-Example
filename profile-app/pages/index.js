73af388fc7366dca98da5b6a06bfcfe5ef8d8f8c
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2020-06-10 17:00:25 +0700

    add carousel & movie data

--- profile-app/pages/index.js
+++ profile-app/pages/index.js
@@ -1,36 +1,45 @@
-import SideMenu from '../components/UI/Sidemenu/Sidemenu'
-import Carousel from '../components/UI/Carousel/Carousel'
-import Movielist from '../components/UI/Movielist/Movielist'
-import { getMovies } from '../actions/index'
-import React, { useState, useEffect } from 'react'
+import SideMenu from "../components/UI/Sidemenu/Sidemenu";
+import Carousel from "../components/UI/Carousel/Carousel";
+import Movielist from "../components/UI/Movielist/Movielist";
+import { getMovies } from "../actions/index";
+import React, { useState, useEffect } from "react";
 const Home = (props) => {
+  const {images} = props
+  return (
+    <div>
+      <div className="home-page">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3">
+              <SideMenu appName={"Movie DB"} />
+            </div>
 
-  return  (
-  <div>
-<div className="home-page">
-<div className="container">
-  <div className="row">
-
-    <div className="col-lg-3">
-      <SideMenu />
-    </div>
-
-    <div className="col-lg-9">
-      <Carousel />
-      <div className="row">
-        <Movielist movies={props.movies || []} />
+            <div className="col-lg-9">
+              <Carousel images={images} />
+              <div className="row">
+                <Movielist movies={props.movies || []} />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-</div>
-</div>
-)
-}
+  );
+};
+
+
 Home.getInitialProps = async () => {
-  const movies = await getMovies()
+  const movies = await getMovies();
+  const images = movies.map((movie)=> {
+    return {
+      id: `image-${movie.id}`,
+      url: movie.image,
+      name: movie.name
+    }
+  })
   return {
-    movies
-  }
-}
-export default Home
+    movies,
+    images
+  };
+};
+export default Home;
