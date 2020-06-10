f5a16d680a554c09f0ccf5d522260477b1fc5f1f
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2020-06-10 23:50:28 +0700

    add create movie modal

--- profile-app/components/UI/Sidemenu/SideMenu.js
+++ profile-app/components/UI/Sidemenu/SideMenu.js
@@ -1,8 +1,10 @@
 import {useState} from 'react'
+import Modal from '../Modal/Modal'
 const SideMenu = (props) => {
   const { categories } = props;
   return (
     <div>
+      <Modal />
       <h1 className="my-4">{props.appName}</h1>
       <div className="list-group">
         {categories.map((c) => (
